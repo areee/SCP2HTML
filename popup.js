92fd23204975bcb54691bce02959f9e72e20d73b
areee <10089872+areee@users.noreply.github.com>
2021-11-14 20:31:09 +0200

    Add the starting point for executing a script

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
 let save2Html = document.getElementById("save2Html");
 
 save2Html.addEventListener("click", async () => {
-  console.log("Save button is clicked");
   let [tab] = await chrome.tabs.query({
     active: true,
     currentWindow: true,
   });
 
-  console.log("tab", tab);
-
-  // TODO: Continue!
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: saveCurrentPageAsHtml,
+  });
 });
+
+function saveCurrentPageAsHtml() {
+  // TODO: Get some help from 'https://github.com/fool2fish/snapshot-as-html' (MIT license)
+}
